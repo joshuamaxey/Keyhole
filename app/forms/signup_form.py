5f74dffa639a28b53f8f04f896b8ff9b623d78aa
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-02-27 14:08:47 -0600

    modified login and signup forms to utilize username instead of email for querying and validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,27 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, ValidationError
 from app.models import User
 
 
 def user_exists(form, field):
     # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if user:
-        raise ValidationError('Email address is already in use.')
-
-
-def username_exists(form, field):
-    # Checking if username is already in use
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
         raise ValidationError('Username is already in use.')
 
 
+def password_exists(form, field):
+    # Checking if password is provided
+    password = field.data
+    if not password:
+        raise ValidationError('Password is required.')
+
+
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), user_exists])
+    password = StringField('password', validators=[DataRequired(), password_exists])
