a687ab776e4e04248d2a900091d79e091ecfdc37
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-03-03 20:01:05 -0600

    Updated foreign keys in comment_like model to use 'add_prefix_for_prod' prefix

--- app/models/comment_like.py
+++ app/models/comment_like.py
@@ -5,8 +5,8 @@ class CommentLike(db.Model):
     __tablename__ = 'comment_likes'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
+    comment_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('comments.id'), ondelete='CASCADE'), nullable=False)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
 
